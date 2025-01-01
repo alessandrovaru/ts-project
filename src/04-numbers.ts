6ca9d7e336eff8c0413659259df308a0327abbdd
Alessandro <alessandrovaru@gmail.com>
2025-01-01 17:31:05 -0400

    Add TypeScript file for number manipulation examples

---
+++ src/04-numbers.ts
@@ -0,0 +1,23 @@
+(()=>{
+  let myProductPrice: number = 100;
+  myProductPrice = 200;
+  console.log(myProductPrice);
+
+  let costumerAge: number = 18;
+  costumerAge = costumerAge + 1;
+
+  let myProductStock: number = 100;
+  console.log(myProductStock);
+  if (myProductStock > 10) {
+    console.log('Product is greater than 10');
+  }
+
+  let discount = parseInt('10');
+  console.log('discount', discount);
+
+  let hex = 0xf00d;
+  console.log('hex', hex);
+  let binary = 0b1010;
+  console.log('binary', binary);
+
+})()
